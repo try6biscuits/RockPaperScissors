7c1c3e0d45cd31b3dcceaec919dc5dd119a2b469
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2023-12-30 14:35:53 -0600

    restarting

--- myjavascript.js
+++
@@ -1,7 +0,0 @@
-function  getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors']
-    let randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-}
-console.log( getComputerChoice())
-
