d1cb324d03132b2f9a52c31f10c1b1e817bcb934
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-08 18:11:31 -0600

    Return random choice couldn't play the game without computer returning a choice

--- myjavascript.js
+++ myjavascript.js
@@ -2,27 +2,32 @@ function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     const randomChoice = choices[randomIndex]
-    console.log(randomChoice)
+    return(randomChoice)
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase
     if (playerSelection === computerSelection) {
-        console.log("It's a tie! Retrying the round!");
+        return("It's a tie! Retrying the round!");
         playRound(playerSelection, computerSelection)
     }
 
     else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
        (playerSelection === 'paper' && computerSelection === 'rock' ) ||
        (playerSelection === 'scissors' && computerSelection === 'paper' )) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        return(`You win! ${playerSelection} beats ${computerSelection}`)
        }
        
     else if ((playerSelection === 'scissors' && computerSelection === 'rock') ||
     (playerSelection === 'rock' && computerSelection === 'paper' ) ||
     (playerSelection === 'paper' && computerSelection === 'scissors' )) {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        return(`You lose! ${computerSelection} beats ${playerSelection}`)
     }
 
 }
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
