55cca23b15983c9cfa1737c3811467c097c64c61
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-08 18:07:31 -0600

    add else if statments to account for ties and loss

--- myjavascript.js
+++ myjavascript.js
@@ -8,11 +8,21 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie! Retrying the round!");
+        playRound(playerSelection, computerSelection)
+    }
 
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
        (playerSelection === 'paper' && computerSelection === 'rock' ) ||
        (playerSelection === 'scissors' && computerSelection === 'paper' )) {
-        return(`You win! ${playerSelection} beats ${computerSelection}`)
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
        }
+       
+    else if ((playerSelection === 'scissors' && computerSelection === 'rock') ||
+    (playerSelection === 'rock' && computerSelection === 'paper' ) ||
+    (playerSelection === 'paper' && computerSelection === 'scissors' )) {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+    }
 
 }
